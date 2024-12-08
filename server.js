a3b8a796ba13feca6c9d489f9bfe3b7facbc1e72
Sara <saborsara.wad@gmail.com>
2024-12-08 17:23:31 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    // origin: 'https://book-store-b1dk.onrender.com',
-    origin: 'http://localhost:3000',
+    origin: 'https://book-store-b1dk.onrender.com',
+    // origin: 'http://localhost:3000',
     credentials: true, //* to let the cookies work,
     allowedHeaders: ['Authorization', 'Content-Type']
 }));
